3c012ea175610638fec9b9ed995cc75b7828f117
Nolfi <sourcebans.nolfi@gmail.com>
2025-01-24 10:19:31 +0100

    Updated Pokemon constructor to use strings only

--- Pokemon.cs
+++ Pokemon.cs
@@ -7,12 +7,17 @@ class Pokemon
     public string Type { get; set; }
     public int StrengthLevel { get; set; }
 
-    public Pokemon(int id, string name, string type, int strengthLevel)
+    public Pokemon(string id, string name, string type, string strengthLevel)
     {
-        ID = id;
+        var _id = 0;
+        var _strengthLevel = 0;
+        int.TryParse(id, out _id);
+        int.TryParse(strengthLevel, out _strengthLevel);
+
+        ID = _id;
         Name = name;
         Type = type;
-        StrengthLevel = strengthLevel;
+        StrengthLevel = _strengthLevel;
     }
 }
 
